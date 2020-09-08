db5b597e21ea6bb3ed3173b8cf28c251645faca4
daniel-takacs <takacsd89@gmail.com>
2020-09-08 10:28:23 +0300

    newline added

--- battleship.py
+++ battleship.py
@@ -7,17 +7,19 @@ def printing_score ():
   print("Score:", computer_score,"-", user_score)
   print("-------------")
 
+#main-text
 print("|----------------------------|")
 print("|--- // Battleship game //---|")
 print("|----------------------------|")
 
+#board printing
 board = []
 
 for i in range(5):
   board.append(['0']*5,)
     
   def print_board(board):
-      print(" ", " ".join("ABCDE"))
+      print("\n ", " ".join("ABCDE"))
       for letter, row in zip("12345", board):
           print(letter, " ".join(row))
 
@@ -28,6 +30,7 @@ computer_score = 0
 user_score = 0
 
 letters = ['A', 'B','C','D','E','a','b','c','d','e']
+
 
 letters_to_numbers = {
     'A': 0,
@@ -42,46 +45,50 @@ letters_to_numbers = {
     'e': 4,
 }
 
-#main_program
-computer_row = random.randint(1,5)
+#ai random guess
+computer_row = (random.randint(1,5)-1)
 computer_col = random.choice(string.ascii_letters[0:4])
 
+#game (turn to 5)
 for turn in range(5):
     print("\n")
     turn += 1
     print("Turn", turn)
 
+#asking user for guess
     user_row = input("Select row (1-5): ")
     while user_row not in "12345":
-        print("Out of see! Choose 1, 2, 3, 4 or 5")
+        print("\nOut of see! Choose 1, 2, 3, 4 or 5")
         user_row = input("Select row (1-5): ")
 
     user_col = input("Select column (A-E): ")
     while user_col not in "ABCDEabcde":
-        print("Out of see! Choose A, B, C, D, E or a, b, c, d, e")
+        print("\nOut of see! Choose A, B, C, D, E or a, b, c, d, e")
         user_col = input("Select column (A-E): ")
     user_row = int(user_row)
 
+#letter input converting to number
     column_number = letters_to_numbers[user_col]
     column_number_ai = letters_to_numbers[computer_col]
 
     if user_row == computer_row and user_col == computer_col:
-      print("You sank my Battleship!")
+      print("\nYOU SANK MY BATTLESHIP!")
       user_score += 1
       printing_score()
       break
     else: 
       computer_score += 1
-      print("You missed my Battleship!")
+      print("\nYOU MISSED MY BATTLESHIP!")
       board[(user_row)-1][column_number] = "X"
       print_board(board)
       printing_score()
 
+#compare the scores and printing who won
 if computer_score > user_score:
   board[computer_row][column_number_ai] = "-"
   print_board(board)
-  print ("Game Over!")
+  print ("\nGAME OVER!")
 elif computer_score == user_score:
-  print("Tie Game")
+  print("\nTie Game")
 else:
-  print("You win")
+  print("\nYOU WIN")
